5f56064012a5be8cf701d928c5b3ec5089bc8894
Balychev Kiril <mister.balychev@gmail.com>
2024-11-17 21:22:10 +0200

    Created ApplicationDbContext

---
+++ MovieGO/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MovieGO.Data
+{
+    public class ApplicationDbContext(
+       DbContextOptions<ApplicationDbContext> options,
+       IOptions<AuthorizationOptions> authOptions) : DbContext(options)
+    {
+        
+    }
+}
