97bdee76ff41f3a150d22c09fac5e94c7cfadee7
Balychev Kiril <mister.balychev@gmail.com>
2024-11-17 21:52:56 +0200

    Created RolePermissionEntity and UserRoleEntity for correct db work

---
+++ MovieGO/Entities/RolePermissionEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieGO.Entities
+{
+    public class RolePermissionEntity
+    {
+        public int RoleId { get; set; }
+
+        public int PermissionId { get; set; }
+    }
+}
---
+++ MovieGO/Entities/UserRoleEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieGO.Entities
+{
+    public class UserRoleEntity
+    {
+        public int UserId { get; set; }
+
+        public int RoleId { get; set; }
+
+    }
+}
