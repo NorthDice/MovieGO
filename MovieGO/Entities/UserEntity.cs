dd8ededc8f06244147780c17c81859ae4d15ef73
Balychev Kiril <mister.balychev@gmail.com>
2024-11-17 21:31:54 +0200

    Created Entities and implemented them

---
+++ MovieGO/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Security;
+
+namespace MovieGO.Entities
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public ICollection<RoleEntity> Roles { get; set; } = [];
+    }
+}
