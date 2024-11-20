4889ad3a4386f90ea64a4b6fc7bb612e8b9273c5
Balychev Kiril <mister.balychev@gmail.com>
2024-11-20 14:08:56 +0200

    implemented userconfiguration

--- MovieGO/Configurations/UserConfiguration.cs
+++ MovieGO/Configurations/UserConfiguration.cs
@@ -1,6 +1,20 @@
-﻿namespace MovieGO.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieGO.Entities;
+
+namespace MovieGO.Configurations
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
     {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.HasKey(user => user.Id);
+
+            builder.HasMany(user => user.Roles)
+                   .WithMany(role => role.Users)
+                   .UsingEntity<UserRoleEntity>(
+                        l => l.HasOne<RoleEntity>().WithMany().HasForeignKey(r => r.RoleId),
+                        r => r.HasOne<UserEntity>().WithMany().HasForeignKey(l => l.UserId));
+        }
     }
 }
