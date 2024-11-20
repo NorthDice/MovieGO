d51b551cf40fd636b66668cab736c1f8d6b7a28d
Balychev Kiril <mister.balychev@gmail.com>
2024-11-20 14:15:49 +0200

    Implemented PermissionConfiguration

--- MovieGO/Configurations/PermissionConfiguration.cs
+++ MovieGO/Configurations/PermissionConfiguration.cs
@@ -1,6 +1,25 @@
-﻿namespace MovieGO.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieGO.Entities;
+using MovieGO.Enums;
+
+namespace MovieGO.Configurations
 {
-    public class PermissionConfiguration
+    public class PermissionConfiguration : IEntityTypeConfiguration<PermissionEntity>
     {
+        public void Configure(EntityTypeBuilder<PermissionEntity> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            var permissions = Enum
+                .GetValues<Permissions>()
+                .Select(p => new PermissionEntity
+                {
+                    Id = (int)p,
+                    Name = p.ToString()
+                });
+
+            builder.HasData(permissions);
+        }
     }
 }
