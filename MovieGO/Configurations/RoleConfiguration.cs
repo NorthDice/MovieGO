1e6f377165ce68458a369236aa72cf418b33a8a7
Balychev Kiril <mister.balychev@gmail.com>
2024-11-20 14:25:21 +0200

    Implemented RoleConfiguraton

--- MovieGO/Configurations/RoleConfiguration.cs
+++ MovieGO/Configurations/RoleConfiguration.cs
@@ -1,8 +1,36 @@
-﻿namespace MovieGO.Configurations
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieGO.Entities;
+using MovieGO.Enums;
+using System.Xml.Linq;
+
+namespace MovieGO.Configurations
 {
-    public class RoleConfiguration
+    public class RoleConfiguration : IEntityTypeConfiguration<RoleEntity>
     {
+        public void Configure(EntityTypeBuilder<RoleEntity> builder)
+        {
+            builder.HasKey(r => r.Id);
 
+            builder.HasMany(r => r.Permissions)
+                .WithMany(p => p.Roles)
+                .UsingEntity<RolePermissionEntity>(
+                    j => j.HasOne(rp => rp.Permission).WithMany().HasForeignKey(j => j.PermissionId),
+                    j => j.HasOne(rp => rp.Role).WithMany().HasForeignKey(j => j.RoleId),
+                    j =>
+                    {
+                        j.HasKey(rp => new { rp.RoleId, rp.PermissionId });
+                    });
 
+            var roles = Enum
+                .GetValues<Role>()
+                .Select(r => new RoleEntity
+                {
+                    Id = (int)r,
+                    Name = r.ToString()
+                });
+
+            builder.HasData(roles);
+        }
     }
 }
