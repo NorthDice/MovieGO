271b65bf8c855beb30e0fad5a3cdf1eaff691727
Balychev Kiril <mister.balychev@gmail.com>
2024-11-21 20:41:50 +0200

    Created and implemented User

---
+++ MovieGO/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieGO.Models
+{
+    public class User
+    {
+        private User(int id, string userName, string passwordHash, string email)
+        {
+            Id = id;
+            UserName = userName;
+            PasswordHash = passwordHash;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+
+        public string UserName { get; private set; }
+
+        public string PasswordHash { get; private set; }
+
+        public string Email { get; private set; }
+
+        public static User Create(int id, string username, string passwordHash, string email)
+        {
+            return new User(id,username,passwordHash,email);   
+        }
+    }
+}
