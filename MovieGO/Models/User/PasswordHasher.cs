0b3a578e1d424dbd00ce12bac7193464dac1e857
Balychev Kiril <mister.balychev@gmail.com>
2024-11-21 21:09:33 +0200

    Implemented Generate and Verify for PasswordHasher

--- MovieGO/Models/User/PasswordHasher.cs
+++ MovieGO/Models/User/PasswordHasher.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using MovieGO.Interfaces;
 
 namespace MovieGO.Models.User
 {
@@ -6,5 +7,8 @@ namespace MovieGO.Models.User
     {
         public string Generate (string password) =>
             BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+
+        public bool Verify(string password, string hashedPassword) =>
+            BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
     }
 }
