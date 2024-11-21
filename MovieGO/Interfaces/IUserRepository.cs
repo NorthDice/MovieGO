086c9f52e27a492a678c8a1e0d8129bbfb2d7abf
Balychev Kiril <mister.balychev@gmail.com>
2024-11-21 20:48:51 +0200

    Created interface IUserRepository and Implement it

---
+++ MovieGO/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using MovieGO.Models;
+
+namespace MovieGO.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Add(User user);
+        Task<User> GetByEmail(string email);
+    }
+}
---
+++ MovieGO/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MovieGO.Data;
+using MovieGO.Entities;
+using MovieGO.Models;
+
+namespace MovieGO.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task Add(User user)
+        {
+            var roleEntity = await _context.Roles
+            .SingleOrDefaultAsync(r => r.Id == (int)Role.User)
+            ?? throw new InvalidOperationException("User role not found in the database.");
+
+
+
+            var userEntity = new UserEntity()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                PasswordHash = user.PasswordHash,
+                Email = user.Email,
+            };
+
+            await _context.Users.AddAsync(userEntity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            var userEntity = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email) ?? throw new NotImplementedException();
+
+            return _mapper.Map<User>(userEntity);
+        }
+    }
+}
