ab879580db82b9b17af9bdc084f928b629249beb
Balychev Kiril <mister.balychev@gmail.com>
2024-11-23 20:51:05 +0200

    Updated controller and api extensions

--- MovieGO/Controllers/AuthorizationController.cs
+++ MovieGO/Controllers/AuthorizationController.cs
@@ -20,6 +20,8 @@ namespace MovieGO.Controllers
         {
             var token = await _userService.Login(request.Email, request.Password);
 
+            Response.Cookies.Append("MovieGo",token);
+
             return Results.Ok(token);
 
 
--- MovieGO/Extensions/ApiExtensions.cs
+++ MovieGO/Extensions/ApiExtensions.cs
@@ -1,11 +1,32 @@
-﻿namespace MovieGO.Extensions
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieGO.Models.Provider;
+using System.Text;
+
+namespace MovieGO.Extensions
 {
     public class ApiExtensions
     {
-        public static AddApiAuthentication(
+        public static void AddApiAuthentication(
             IServiceCollection services,
-            IConfiguration configuration )
+            IOptions<JwtOptions> jwtOptions)
         {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey))
+                    };
+                });
+
+            services.AddAuthorization();
 
         }
     }
