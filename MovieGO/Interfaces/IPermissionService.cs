56a04aa9343d618bb21c02d90e851d1678e6cbd4
Balychev Kiril <mister.balychev@gmail.com>
2024-11-25 14:46:53 +0200

    Implemented interface IPermissionService

---
+++ MovieGO/Interfaces/IPermissionService.cs
@@ -0,0 +1,9 @@
+﻿using MovieGO.Enums;
+
+namespace MovieGO.Interfaces
+{
+    public interface IPermissionService
+    {
+        Task<HashSet<Permissions>> GetPermissionsAsync(Guid userId);
+    }
+}
