6e6f5df6fb242a607a72be0517820e6486e33926
Balychev Kiril <mister.balychev@gmail.com>
2024-11-25 14:28:22 +0200

    Updated requirements ctor

--- MovieGO/Authentication/RolePermissionRequirement.cs
+++ MovieGO/Authentication/RolePermissionRequirement.cs
@@ -4,6 +4,11 @@ namespace MovieGO.Authentication
 {
     public class RolePermissionRequirement : IAuthorizationRequirement
     {
+        public RolePermissionRequirement(Permissions[] permissions)
+        {
+            Permissions = permissions;
+        }
+
         public Permissions[] Permissions { get; set; } = [];
     }
 }
