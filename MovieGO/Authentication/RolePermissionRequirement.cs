11be8cf26f1b74925002dca20938ab9b6997be57
Balychev Kiril <mister.balychev@gmail.com>
2024-11-25 14:25:42 +0200

    Created and implemented RolePermissionRequirement

---
+++ MovieGO/Authentication/RolePermissionRequirement.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using MovieGO.Enums;
+namespace MovieGO.Authentication
+{
+    public class RolePermissionRequirement : IAuthorizationRequirement
+    {
+        public Permissions[] Permissions { get; set; } = [];
+    }
+}
